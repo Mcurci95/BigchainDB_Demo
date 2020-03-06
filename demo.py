da2f893439a845f282647187acdac9191fa82554
Mcurci95 <mcurci95@gmail.com>
2020-03-05 19:52:30 -0800

    Asset creation and transfer

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair
+from time import sleep
 
-bdb_root_url = 'http://bdb-server:9984'
+bdb_root_url = 'localhost:9984'
 
 
 bdb = BigchainDB(bdb_root_url)
@@ -45,4 +46,69 @@ prepared_creation_tx = bdb.transactions.prepare(
     metadata=metadata,
 )
 
+print("\nPrepared transactions: ")
 print(prepared_creation_tx)
+
+
+# Fulfill the creation. Using Marco's private key as a signature
+fulfilled_creation_tx = bdb.transactions.fulfill(
+    prepared_creation_tx, private_keys=marco.private_key
+)
+
+# Sending over fulfilled creation to BigchainDB
+sent_creation_tx = bdb.transactions.send_commit(fulfilled_creation_tx)
+
+# Retreive the transactions id
+txid = fulfilled_creation_tx['id']
+print(f"\ntransactions ID: {txid}")
+
+
+sleep(10)
+
+print("\nChecking to ensure transactions was successfully included in a block")
+block_height = bdb.blocks.get(txid=txid)
+print(f"\nBlock height: {block_height}")
+block = bdb.blocks.retrieve(str(block_height))
+print(f"\nBlock that contains transactions: {block}")
+
+# Transfer
+print("Now that the car is created. We can transfer it.")
+
+# New transactions uses the creation_transactions id
+creation_tx = bdb.transactions.retrieve(txid)
+
+# Get ID of the asset
+asset_id = creation_tx['id']
+transfer_asset = {
+    'id' : asset_id,
+}
+
+output_index = 0
+output = creation_tx['outputs'][output_index]
+transfer_input = {
+    'fulfillment': output['condition']['details'],
+    'fulfills': {
+        'output_index': output_index,
+        'transaction_id': creation_tx['id'],
+    },
+    'owners_before': output['public_keys'],
+}
+
+# Transfer to Dennis
+prepared_transfer_tx = bdb.transactions.prepare(
+    operation='TRANSFER',
+    asset=transfer_asset,
+    inputs=transfer_input,
+    recipients=dennis.public_key,
+)
+
+# Fulfill the transacation by using Marco's private key
+fulfilled_transfer_tx = bdb.transactions.fulfill(
+    prepared_transfer_tx,
+    private_keys=marco.private_key,
+)
+
+print("\nFulfilled Transfer")
+print(f"\t{fulfilled_transfer_tx}")
+
+sent_transfer_tx = bdb.transactions.send_commit(fulfilled_transfer_tx)
