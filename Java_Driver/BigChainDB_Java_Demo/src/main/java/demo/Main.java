a72e6287db08442169aa54e34f17e495dfe1a210
mcurci95 <mcurci95@gmail.com>
2020-04-16 22:39:26 -0700

    Timing MongoDB inserts

--- Java_Driver/BigChainDB_Java_Demo/src/main/java/demo/Main.java
+++ Java_Driver/BigChainDB_Java_Demo/src/main/java/demo/Main.java
@@ -1,7 +1,10 @@
 package demo;
 
 
+import Connections.BigChainDBDriver;
+import Connections.MongoDBDriver;
 import models.Weather;
+import org.bson.Document;
 import utils.WeatherFileReader;
 
 import java.util.ArrayList;
@@ -11,16 +14,41 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        DemoDriver demo = new DemoDriver();
-////
-////        demo.run();;
+//        BigChainDBDriver demo = new BigChainDBDriver();
+        MongoDBDriver mongo = new MongoDBDriver();
+        mongo.run();
 
         // Time for 365 inserts in Nano seconds: 2409386068
         // Weather file
-        List<Map<String, String>> weatherList = new ArrayList<Map<String, String>>();
+        List<Weather> weatherList = new ArrayList<Weather>();
         WeatherFileReader weatherFileReader = new WeatherFileReader();
 
         weatherList = weatherFileReader.ReadFile("src/main/java/resources/01002099999.csv");
-        demo.run(weatherList);
+
+
+        // BigChain Asset
+//        List<Map<String, String>> weatherAsset = new ArrayList<Map<String, String>>();
+
+        // MongoDB Document
+        List<Document> weatherDocument = new ArrayList<Document>();
+
+        for (Weather weather : weatherList) {
+//            weatherAsset.add(weather.transformIntoAsset());
+            weatherDocument.add(weather.transformIntoDocument());
+        }
+
+        long start = System.nanoTime();
+
+        // Mongo time: 43530736541 2409386068
+        for (Document weatherDoc : weatherDocument) {
+            mongo.insert(weatherDoc);
+        }
+
+        long end = System.nanoTime();
+
+        long total = end - start;
+        System.out.println(total);
+
+//        demo.run(weatherList);
     }
 }
