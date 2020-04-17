2f4a1cef364db4eb675669ee6f162797e61d7411
mcurci95 <mcurci95@gmail.com>
2020-04-16 22:39:10 -0700

    Connection and insert

--- Java_Driver/BigChainDB_Java_Demo/src/main/java/Connections/MongoDBDriver.java
+++ Java_Driver/BigChainDB_Java_Demo/src/main/java/Connections/MongoDBDriver.java
@@ -1,28 +1,35 @@
 package Connections;
 
+import com.mongodb.DBCollection;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import config.Constants;
+import models.Weather;
+import org.bson.Document;
 
 public class MongoDBDriver {
 
     MongoClient mongoClient;
     MongoDatabase database;
+    MongoCollection collection;
 
     public void run() {
         connect();
         database.getCollection("weather");
-
     }
 
     public void connect() {
         this.mongoClient = MongoClients.create(
-                "mongodb+srv://mongo:" + Constants.MONGO_PASS + "@devconnector-deczi.mongodb.net/test?retryWrites=true&w=majority");
+                "mongodb+srv://" + Constants.MONGO_USER + ":" + Constants.MONGO_PASS + "@devconnector-deczi.mongodb.net/test?retryWrites=true&w=majority");
         this.database = mongoClient.getDatabase("cecs574");
+        this.collection =  database.getCollection("weather");
     }
 
 
-
+    public void insert(Document weatherDoc) {
+        collection.insertOne(weatherDoc);
+    }
 
 }
