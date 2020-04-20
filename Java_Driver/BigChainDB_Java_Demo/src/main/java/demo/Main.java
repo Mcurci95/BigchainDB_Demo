db9cbad11500a27bf576fc0cf1097987c2f6dd46
mcurci95 <mcurci95@gmail.com>
2020-04-19 22:14:26 -0700

    Timing for insert one and insert many

--- Java_Driver/BigChainDB_Java_Demo/src/main/java/demo/Main.java
+++ Java_Driver/BigChainDB_Java_Demo/src/main/java/demo/Main.java
@@ -14,6 +14,8 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        // QLDB 365 inserts: 22063110000
 //        BigChainDBDriver demo = new BigChainDBDriver();
         MongoDBDriver mongo = new MongoDBDriver();
         mongo.run();
@@ -39,21 +41,23 @@ public class Main {
 
         long start = System.nanoTime();
 
-        // Mongo time: 43530736541 2409386068
-//        for (Document weatherDoc : weatherDocument) {
-//            mongo.insert(weatherDoc);
-//        }
+        // Mongo time: 43530736541
+        // Mongo connecting to local docker: 942546721
+        for (Document weatherDoc : weatherDocument) {
+            mongo.insert(weatherDoc);
+        }
 
         long end = System.nanoTime();
 
         long total = end - start;
-        System.out.println(total);
+        System.out.println("Mongo Docker: " + total);
 
         // Mongo Insert many
         start = System.nanoTime();
         mongo.insertMany(weatherDocument);
         end = System.nanoTime();
         total = end - start;
+        System.out.println("Many:" + total);
 //        demo.run(weatherList);
     }
 }
